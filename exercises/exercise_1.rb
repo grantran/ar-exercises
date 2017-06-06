a05ca5a671a2fae05ec7e7059b8c6df05993fa35
grantran <grant.tran@gmail.com>
2017-06-06 16:38:48 +0000

    exercise 1 ; change applicationrecord to activerecord::base

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,23 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# Rails 5: ApplicationRecord, Rails 4: ActiveRecord::Base
+class Store < ActiveRecord::Base
+
+  burnaby = 
+  Store.create(name: 'Burnaby', annual_revenue: 300000, 
+  mens_apparel: true,
+  womens_apparel: true)  
+
+  richmond = 
+  Store.create(name: 'Richmond', annual_revenue: 1260000, 
+  mens_apparel: false,
+  womens_apparel: true)
+
+  gastown = 
+  Store.create(name: 'Gastown', annual_revenue: 190000, 
+  mens_apparel: true,
+  womens_apparel: false)
+end
+
+puts Store.count('id')
